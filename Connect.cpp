74b7441e843fe39af22a65d27d1805e78f2429f0
jjsanderson <lists@quernstone.com>
2020-04-15 11:51:54 +0100

    End of session 2020-04-15

--- Connect.cpp
+++ Connect.cpp
@@ -1,33 +1,9 @@
 // Save this to Libraries directory
 
-#include "Arduino.h"
-#include "Kniwwelino.h"
-#include "Connect.h"
-#include "arduino_secrets.h"
+#include "Connect.h" // Includes Kniwwelino.h and, via that, Arduino.h
 
 #define WIFI_ON 1
 
-// Below set to defaults, will need overriding for conNecT
-// (these are defined in Kniwwelino.h)
-// TODO: check Kniwwelino.MQTTsetup()
-#define DEF_UPDATESERVER		"connect.nustem.uk"
-#define DEF_MQTTSERVER		 	"connect.nustem.uk"
-#define DEF_MQTTPORT			1883
-#define DEF_MQTTUSER			"connect"
-#define DEF_MQTTPW		        "MQTT_PASS"
-#define DEF_MQTTPUBLICDELAY		300
-#define DEF_MQTTBASETOPIC		"kniwwelino/"
-#define DEF_FWUPDATEURL      	"/updateFW"
-#define DEF_CONFUPDATEURL       "/updateConf"
-
-#define NTP_SERVER			  	"uk.pool.ntp.org"
-#define NTP_PORT			  	8888
-// TODO: work out what this is and if it's used:
-// (NTP_TIMEZONE doesn't appear to be used by Kniwwelino or Timezone libraries.
-// Check against actual time received. May need to do something clever with BST,
-// But that should be baked into the Timezone library if we call it correctly.)
-#define NTP_TIMEZONE			1 
-#define NTP_PACKET_SIZE			48 // NTP time is in the first 48 bytes of message
 
 int my_mood;
 String my_icon;
@@ -39,20 +15,38 @@ String received_string;
 Connect::Connect(int pin)
 {
     // Setup stuff here
+    int my_mood;
+    String my_icon;
+    String network_mood;
+    String displayed_mood;
+    String received_string;
 }
 
-void Connect::method()
-{
-    // Class method code
-}
+// void Connect::method()
+// {
+//     // Class method code
+// }
 
-void connectInit() {
+// Call in setup()
+void Connect::begin() {
+    Kniwwelino.begin("connect_object", WIFI_ON, true, false); // Wifi=true, Fastboot=true, MQTT Logging=false
+    Serial.begin(115200);
+    Serial.println();
+    Serial.println(F("START " __FILE__ "\r\nVersion " VERSION " from " __DATE__));
+
+    #if WIFI_ON
+        Kniwwelino.MQTTsetGroup(String("NUSTEM"));
+        Kniwwelino.MQTTonMessage(messageReceived);
+        Kniwwelino.MQTTsubscribe("MESSAGE");
+        Kniwwelino.MQTTsubscribe("MOOD");
+    #endif
+
     Kniwwelino.RGBsetBrightness((int)200);
     Kniwwelino.RGBclear();
     Kniwwelino.MATRIXdrawIcon(ICON_SMILE);
     #if WIFI_ON
     Kniwwelino.MQTTpublish("hello_my_name_is", String(Kniwwelino.getMAC()));
     #endif
-    int my_mood = 0;
+    my_mood = 0;
 
 }
--- Connect.h
+++ Connect.h
@@ -9,19 +9,50 @@
 #ifndef Connect_h
 #define Connect_h
 
-#include "Kniwwelino.h"
+#include "Kniwwelino.h" // Includes Arduino.h for us
+#include <ServoEasing.h>
+#include "arduino_secrets.h" // #define MQTT_PASS, .gitignored
 
-class Connect
-{
-    public:
-        Connect(int pin); //root object, needs variable stubs
-        void func1();
-        void func2();
-    private:
-        int _pin;
+#define WIFI_ON 1
+
+// Redefine MQTT stuff previously set in Kniwwelino.h
+// TODO: check Kniwwelino.MQTTsetup()
+#define DEF_UPDATESERVER		"connect.nustem.uk"
+#define DEF_MQTTSERVER		 	"connect.nustem.uk"
+#define DEF_MQTTPORT			1883
+#define DEF_MQTTUSER			"connect"
+#define DEF_MQTTPW		        "MQTT_PASS"
+#define DEF_MQTTPUBLICDELAY		300
+#define DEF_MQTTBASETOPIC		"kniwwelino/"
+#define DEF_FWUPDATEURL      	"/updateFW"
+#define DEF_CONFUPDATEURL       "/updateConf"
+
+#define NTP_SERVER			  	"uk.pool.ntp.org"
+#define NTP_PORT			  	8888
+// TODO: work out what this is and if it's used:
+// (NTP_TIMEZONE doesn't appear to be used by Kniwwelino or Timezone libraries.
+// Check against actual time received. May need to do something clever with BST,
+// But that should be baked into the Timezone library if we call it correctly.)
+#define NTP_TIMEZONE			1 
+#define NTP_PACKET_SIZE			48 // NTP time is in the first 48 bytes of message
+
+#define HAPPY "B0000001010000001000101110"
+#define SAD "B0000001010000000111010001"
+#define HEART "B0101011111111110111000100"
+#define SKULL "B0111010101111110111001110" 
+#define DUCK "B0110011100011110111000000"
+
+class Connect{
+public:
+    Connect(int pin); //root object, needs variable stubs
+    void begin();
+    // void func1();
+    // void func2();
+private:
+    int _pin;
 };
 
-void connectInit(void);
+void init(void);
 
 
 
