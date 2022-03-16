594d328d967d5dee6d21406440f86555c687ee22
jjsanderson <jonathan.sanderson@northumbria.ac.uk>
2022-03-16 10:20:39 +0000

    Prune exampleBot

--- examples/Connect_exampleBot/Connect_exampleBot.ino
+++ examples/Connect_exampleBot/Connect_exampleBot.ino
@@ -14,8 +14,6 @@ void setup() {
 
     servoD5.setPin(D5);
     servoD7.setPin(D7);
-
-    Kniwwelino.sleep(500);
 }
 
 void loop() {
@@ -23,38 +21,9 @@ void loop() {
 }
 
 void doHappy() {
-    Serial.println(F(">>> New mood received: HAPPY"));
-    servoD5.queueEaseTo(135, EASE_CUBIC_IN_OUT, 60);
     servoD5.wave(3, 135, 45);
-    // servo1.queueMoveTo(0);
-    // servo1.queueWait(250);
-    // servo2.queueMoveTo(0);
-    // servo2.queueWait(250);
-    // servo1.queueEaseTo(180, EASE_CUBIC_IN_OUT, 60);
-    // servo1.queueEaseTo(0, EASE_CUBIC_IN_OUT, 90);
-    // servoWaitForServo(servo2, servo1);
-    // servo1.queueEaseTo(180, EASE_CUBIC_IN_OUT, 60);
-    // servo2.queueEaseTo(180, EASE_CUBIC_IN_OUT, 60);
 }
 
 void doSad() {
-    Serial.println(F(">>> New mood received: SAD"));
     servoD5.queueEaseTo(0, EASE_CUBIC_OUT, 15);
-    // servo2.queueEaseTo(180, EASE_CUBIC_IN_OUT, 20);
-}
-
-void doHeart() {
-    Serial.println(F(">>> New mood received: HEART"));
-}
-
-void doSkull() {
-    Serial.println(F("New mood received: SKULL"));
-}
-
-void doSilly() {
-    Serial.println(F("New mood received: SILLY"));
-}
-
-void doDuck() {
-    Serial.println(F("New mood received: DUCK"));
 }
